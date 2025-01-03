714e2b76f39f15b44e4331ae8ed57957886c3b87
Muhammad Essa Magsi <magsimuhammadessa@gmail.com>
2025-01-04 01:28:31 +0500

    Upwork, Linkedin, Instagram Only

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,11 +16,10 @@ function Hero() {
             <Link to="/contact"><button>LETS TALK</button></Link>
             <div className='social-icons'>
                 <div>
-                    <a href="https://www.instagram.com/"><i className="fa-brands fa-instagram"></i></a>
-                    <a href="https://www.facebook.com/"><i className="fa-brands fa-facebook"></i></a>
-                    <a href="https://www.twitter.com/"><i className="fa-brands fa-twitter"></i></a>
-                    <a href="https://www.linkedin.com/"><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="https://www.discord.com/"><i className="fa-brands fa-discord"></i></a>
+                    <a href="https://www.upwork.com/freelancers/~01f3efa2221fba5677" target="_blank"><i className="fa-brands fa-upwork"></i></a>
+                    <a href="https://www.linkedin.com/in/shaneali110?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank'><i className="fa-brands fa-linkedin"></i></a>
+                    <a href="https://www.instagram.com/shaneali.__" target='_blank'><i className="fa-brands fa-instagram"></i></a>
+                    {/* <a href="https://www.discord.com/"><i className="fa-brands fa-discord"></i></a> */}
                 </div>
             </div>
         </div>
